2f6c41dbb287287b82754e5341b4f4c21a7c1121
Roy  Nunez Jr <roynunezjr@gmail.com>
2020-05-10 13:35:20 -0700

    aster

---
+++ Assets/Scripts/Astroid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astroid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private GameObject _explosionPrefab;
+     
+    void Update()
+    {
+        
+    }
+                                                                                                                                                        
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }            
+                        
+}
