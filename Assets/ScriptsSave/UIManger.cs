cf17fe8fd85228e72989e81d8046341c0861c562
Roy  Nunez Jr <roynunezjr@Roys-Air-6.attlocal.net>
2020-05-10 12:45:21 -0700

    save5

---
+++ Assets/ScriptsSave/UIManger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIManger : MonoBehaviour
+{
+    [SerializeField]
+    private Text _scoreText;
+    [SerializeField]
+    private Image _LivesImg;
+    [SerializeField]
+    private Sprite[] _liveSprites;  
+    [SerializeField]
+    private Text _gameOverText;
+    [SerializeField]
+    private Text _restartLevel;
+    private GameManager _gameManager;
+
+    void Start()
+    {
+        _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+
+        if (_gameManager == null)
+        {
+            Debug.LogError("GameManager is NULL.");
+        }
+    }
+
+    // Update is called once per frame
+    public void UpdateScore(int playerScore)
+    {
+        _scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+        _LivesImg.sprite = _liveSprites[currentLives];
+
+          if (currentLives == 0)
+    {
+       GameOverSequence();
+    }
+    void GameOverSequence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartLevel.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
+    }
+
+}
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while(true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+    }
+}
+
+
---
+++ Assets/ScriptsSave/monoapp.cs
@@ -0,0 +1,10 @@
+using System;                 // Importing namespace
+
+class Test                    // Class declaration
+{
+  static void Main()          // Method declaration
+  {
+    //int x = 12 * 30;          // Statement 1
+    Console.WriteLine ("Hello form mono!");    // Statement 2
+  }                           // End of method
+}
